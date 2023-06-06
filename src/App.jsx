e233379f35ee026eb980f82ee6c99675dc908450
987456987 <donufrak112697@gmail.com>
2023-06-05 23:32:10 -0700

    file move

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import './assets'
+import jsonData from "./data.json"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const timeframe = 'weekly'
+  const dataArray = Object.values(jsonData);
+  
+
 
   return (
     <>
-      
+      <main>
+        <div className='profile-main'>
+          <div className='profile-inner'>
+            <img></img>
+            <p>Report For</p>
+            <h1>Jeremy Robson</h1>
+          </div>
+          <input name='timeframe' type='radio' id='daily' />
+          <label htmlFor='daily'>Daily</label>
+          <input name='timeframe' type='radio' id='weekly' />
+          <label htmlFor='weekly'>Weekly</label>
+          <input name='timeframe' type='radio' id='daily' />
+          <label htmlFor='monthly'>Monthly</label>
+        </div>
+        {dataArray.map((data) => (
+          <div className='activity-main'>
+            <div className='activity-inner'>
+              <h2>{data.title}</h2>
+              <h3>{data.timeframes[timeframe].current}</h3>
+              <p>{data.timeframes[timeframe].previous}</p>
+            </div>
+          </div>
+        ))}
+        
+      </main>
     </>
   )
 }
